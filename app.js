6a27fa8e1125c179223692849031bc131d458b76
summer-79 <programs2ming@gmail.com>
2022-01-30 17:30:56 +0900

    javascript-beginner

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-const h1 = document.querySelector("div.hello:first-child h1");
+const login = document.querySelector("#login-form");
+const loginInput = login.querySelector("input");
+const loginButton = login.querySelector("button");
 
-function handleTitleClick(){
-    const current = h1.className;
-    
-    if(current === "sexy-font active")
-        newClass = "sexy-font";
-    else
-        newClass = "sexy-font active";
-    h1.className = newClass;
+function onLoginBtnClick(){
+    console.log(loginInput.value);
 }
 
-h1.addEventListener("click", handleTitleClick);
-
+loginButton.addEventListener("click", onLoginBtnClick);
