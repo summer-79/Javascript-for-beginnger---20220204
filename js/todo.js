94adb04cae961473f98e27ae3829da88e557c69e
summer-79 <programs2ming@gmail.com>
2022-02-01 00:52:57 +0900

    toDoList

--- js/todo.js
+++ js/todo.js
@@ -2,13 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("input");
 
+function paintToDo(newToDO){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerHTML = newToDO;
+    toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event){
     event.preventDefault();
     const toDo = toDoInput.value;
     localStorage.setItem(toDo, toDo);
-    const toDoElement = document.createElement("li");
-    toDoElement.innerHTML = toDo;
-    toDoList.appendChild(toDoElement);
+    paintToDo(toDo);
     toDoInput.value = "";
 }
 
