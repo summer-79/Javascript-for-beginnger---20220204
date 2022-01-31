8997b9548f26a02b19116d543ee0326ffcefa5cf
summer-79 <programs2ming@gmail.com>
2022-01-31 18:44:13 +0900

    80 percent done

---
+++ js/background.js
@@ -0,0 +1,8 @@
+const bgImgs = ["altai-g80b97c15e_1920.jpg",
+"beach-g2ee2f34ee_1920.jpg", "beach-g3f47fb25c_1920.jpg",
+"chicago-g29105485c_1920.jpg", "drink-gc3d1d01dc_1920.jpg",
+"sea-g51f9e2c3c_1920.jpg", "seashells-g3ad54f9fa_1920.jpg"
+];
+
+const choseImg =bgImg[Math.floor(Math.random() * bgImg.length)];
+
